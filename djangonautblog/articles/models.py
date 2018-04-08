daaba878f5723d8869c4908ff8fe4d676eb9bf93
Zay <zayseoul@gmail.com>
2018-04-08 16:47:09 -0400

    set up migrations for articles app

--- djangonautblog/articles/models.py
+++ djangonautblog/articles/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    
